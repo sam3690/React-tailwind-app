b9a9b8486eaac4d038eaaa796c10eeb3e285979e
sam3690 <usamabinayoub@gmail.com>
2024-01-12 15:28:04 +0500

    Updated 12-01-2024

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ const App = () => (
           <div className="bg-primary w-full overflow-hidden">
             <div className={`${styles.paddingX} ${styles.flexCenter}`}>
               <div className={`${styles.boxWidth}`}>
-                NavBar
+                <NavBar />
               </div>
             </div>
 
             <div className={`bg-primary ${styles.flexStart}`}>
               <div className={`${styles.boxWidth}`}>
-                Hero
+                <Hero />
               </div>
             </div>
 
